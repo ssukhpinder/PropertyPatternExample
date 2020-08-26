aa1858707c98b71e66a0ec5c24b77a52b80890bf
Sukhpinder Singh <sukhpinder.singh@sourcefuse.com>
2020-08-26 10:29:24 +0530

    commit advance pattern

--- PropertyPatternExample/Program.cs
+++ PropertyPatternExample/Program.cs
@@ -7,50 +7,64 @@ namespace PropertyPatternExample
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            ExecutePropertyPattern();
+            var car = new Car();
+            var taxi = new Taxi();
+            var bus = new Bus();
+            var truck = new DeliveryTruck();
+
+
+            Console.WriteLine($"The toll for a car is {TollFare(car)}");
+            Console.WriteLine($"The toll for a taxi is {TollFare(taxi)}");
+            Console.WriteLine($"The toll for a bus is {TollFare(bus)}");
+            Console.WriteLine($"The toll for a truck is {TollFare(truck)}");
+
+
+            Console.WriteLine("=========================================");
+            Console.WriteLine("=========================================");
+
+            var car1 = new Car{ PassengerCount=2};
+            var taxi1 = new Taxi { Fare = 0 };
+            var bus1 = new Bus { Capacity = 100, RidersCount = 30 };
+            var truck1 = new DeliveryTruck { Weight = 30000 };
+
+            Console.WriteLine($"The toll for a car is {OccupancyTypeTollFare(car1)}");
+            Console.WriteLine($"The toll for a taxi is {OccupancyTypeTollFare(taxi1)}");
+            Console.WriteLine($"The toll for a bus is {OccupancyTypeTollFare(bus1)}");
+            Console.WriteLine($"The toll for a truck is {OccupancyTypeTollFare(truck1)}");
         }
 
-        public class Calculate
+        public static int TollFare(Object vehicleType) => vehicleType switch
         {
-            public string multiplyBy { get; set; }
-        }
-
-
-        public class CalculateMultipleProperty
-        {
-            public string multiplyBy { get; set; }
-            public bool isAdditionApplicable { get; set; }
-        }
-
-        public static void ExecutePropertyPattern()
-        {
-            Calculate calculate = new Calculate { multiplyBy = "5 times" };
-            
-            var res = ComputeOverallPrice(calculate, 10M);
-
-            CalculateMultipleProperty calculate1 = new CalculateMultipleProperty { multiplyBy = "5 times", isAdditionApplicable = true };
-            var res1 = ComputeOverallPriceMultiple(calculate1, 10M);
-
-            Console.WriteLine("Result " + res);
-            Console.WriteLine("Result Mutiple Property " + res1);
-        }
-        private static decimal ComputeOverallPrice(Calculate calculate, decimal price) =>
-        calculate switch
-        {
-            { multiplyBy: "10 times" } => 10 * price,
-            { multiplyBy: "5 times" } => 5 * price,
-            { multiplyBy: "20 times" } => 20 * price,
-            _ => 0M
+            Car c => 100,
+            DeliveryTruck d => 200,
+            Bus b => 150,
+            Taxi t => 120,
+            null => 0,
+            { } => 0
         };
 
-
-        private static decimal ComputeOverallPriceMultiple(CalculateMultipleProperty calculate, decimal price) =>
-        calculate switch
+        public static int OccupancyTypeTollFare(Object vehicleType) => vehicleType switch
         {
-            { multiplyBy: "10 times", isAdditionApplicable: true } => 10 * price + 100,
-            { multiplyBy: "5 times", isAdditionApplicable: true } => 5 * price + 50,
-            { multiplyBy: "20 times", isAdditionApplicable: true } => 20 * price + 70,
-            _ => 0M
+            Car { PassengerCount: 0 } => 100 + 10,
+            Car { PassengerCount: 1 } => 100,
+            Car { PassengerCount: 2 } => 100 - 10,
+            Car c => 100 - 20,
+
+            Taxi {Fare:0 }=>100+10,
+            Taxi { Fare: 1 } => 100,
+            Taxi { Fare: 2 } => 100 - 10,
+            Taxi t => 100 - 20,
+
+            Bus b when ((double)b.RidersCount / (double)b.Capacity) < 0.50 => 150 + 30,
+            Bus b when ((double)b.RidersCount / (double)b.Capacity) > 0.90 => 150 - 40,
+            Bus b => 150,
+
+            DeliveryTruck t when (t.Weight > 5000) => 200 + 100,
+            DeliveryTruck t when (t.Weight < 3000) => 200 - 20,
+            DeliveryTruck t => 200,
+
+            null => 0,
+            { } => 0,
         };
     }
 }
